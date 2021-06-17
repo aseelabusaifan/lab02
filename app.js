99d63a8f1c1540c05a404563546c241ec6e9e21a
aseelmohammad1 <>
2021-06-17 18:15:48 +0000

    add css

---
+++ app.js
@@ -0,0 +1,15 @@
+
+//var first name = " aseel "
+//var last name = " abusaifan"
+//var name = first name+last name
+//console.log (name)
+
+// alert ("welome to web site") 
+
+// var age= prompt("what is your age? ")
+
+// if( age == "18"){
+
+// }
+//   else {
+//    alert("sorry") }
