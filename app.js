1288dec7bf342c7b538f11eba4b6dd98255a44ae
aseelmohammad1 <>
2021-06-17 20:53:37 +0000

    add js

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 
-//var first name = " aseel "
-//var last name = " abusaifan"
-//var name = first name+last name
-//console.log (name)
+var first name = " aseel "
+var last name = " abusaifan"
+var name = first name+last name
+console.log (name)
 
-// alert ("welome to web site") 
+alert ("welome to web site") 
 
-// var age= prompt("what is your age? ")
+var age= prompt("what is your age? ")
 
-// if( age == "18"){
+if( age == "18"){
 
-// }
-//   else {
-//    alert("sorry") }
+}
+  else {
+   alert("sorry") }
+
+ while (i < 10) {
+  text += "How do you rate the site? " + i;
+  i++;
+}
+  document.write ("<div>"+<h3>"" name for plant"+"</h3>"+ <img
+ src="https://www.gardeningknowhow.com/wp-content/uploads/2016/05/droopy-snake-plant-393x600.jpg"/>"+" </dive>"
+
+
