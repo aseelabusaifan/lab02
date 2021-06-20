e9814513155c82cd22adfdc2238166be13bdf887
aseelmohammad1 <>
2021-06-20 13:00:20 +0000

    fixed

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 
-//var first name = " aseel "
-//var last name = " abusaifan"
-//var name = first name+last name
-//console.log (name)
+// var first name = " aseel "
+// var last name = " abusaifan"
+// var name = first name+last name
+// console.log (name)
 
-// alert ("welome to web site") 
+alert ("welome to web site") 
 
-// var age= prompt("what is your age? ")
+var age= prompt("what is your age? ")
+age = parseInt(age)
 
-// if( age == "18"){
+while(age<18){
+  age=prompt("you are under 18, what is your age? ")
+}
 
-// }
-//   else {
-//    alert("sorry") }
+
+
+
+
+var stars = prompt " how many stars do you rate webdite?")
+stars = parseInt(stars)
+
+function rating (num){
+  for(var i-0;i<num;i++){
+    document.write('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Golden_star.svg/1200px-Golden_star.svg.png">')
+  }
+}
+
+rating(stars)
